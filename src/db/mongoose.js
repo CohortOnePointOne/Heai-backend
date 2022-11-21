c27661f05ca5ba30968e09f1bbfc683e2b56b530
mayangi aretha <myaretha2620@gmail.com>
2022-11-21 20:39:52 +0300

    setup linting

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-export default function dbConnection () {
-    mongoose.connect(process.env.DB_CONNECT, ()=>{
-       console.log("connected to DataBase")
-   })
+// import dotenv from "dotenv";
+
+export default function dbConnection() {
+  mongoose.connect(process.env.DB_CONNECT, () => {
+    console.log("connected to DataBase");
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import bodyParser from "body-parser";
 import express from "express";
+<<<<<<< HEAD
+=======
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+>>>>>>> a8e56b5 (setup linting)
 import dbConnection from "./db/mongoose.js";
-dbConnection()
 
-const app = express()
+dbConnection();
 
-app.use(bodyParser.json())
-app.get("/", (req, res)=>{
-    res.send("Home Page")
-})
-const PORT = process.env.PORT;
+const app = express();
 
-app.listen(PORT, console.log(`Server running at Port ${PORT}`))
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+const { PORT } = process.env;
+
+app.listen(PORT, console.log(`Server running at Port ${PORT}`));
